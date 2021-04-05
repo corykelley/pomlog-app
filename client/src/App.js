c5eae22dc2b57e07a6c7c35de902f04307625b13
Cory Kelley <coryk0789@gmail.com>
2021-04-05 16:58:55 -0400

    added chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
 import Home from './Home';
 import Task from './components/Task';
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Switch>
-					<Route path='/' exact>
-						<Home />
-					</Route>
-					<Route path='/tasks/:id'>
-						<Task />
-					</Route>
-				</Switch>
-			</Router>
+			<Container>
+				<Router>
+					<Switch>
+						<Route path='/' exact>
+							<Home />
+						</Route>
+						<Route path='/tasks/:id'>
+							<Task />
+						</Route>
+					</Switch>
+				</Router>
+			</Container>
 		</>
 	);
 }
--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { Heading, Text } from '@chakra-ui/layout';
+import { Button, Box } from '@chakra-ui/react';
 
 const TaskItem = ({ task, taskDelete }) => {
 	return (
-		<article>
+		<Box borderRadius='lg' my='8' bg='tomato' w='100%' p={4} color='white'>
 			<Link to={`/tasks/${task.id}`}>
-				<h1>{task.title}</h1>
+				<Heading>{task.title}</Heading>
 			</Link>
-			<h2>{task.description}</h2>
-			<p>{task.time_limit}</p>
-			<p>{moment(task.start_date).format('LL')}</p>
-			<button onClick={() => taskDelete(task.id)}>DELETE</button>
-		</article>
+			<Text>{task.description}</Text>
+			<Text>{task.time_limit}</Text>
+			<Text>{moment(task.start_date).format('LL')}</Text>
+			<Button bg='teal' mt='2' onClick={() => taskDelete(task.id)}>
+				DELETE
+			</Button>
+		</Box>
 	);
 };
 
