e947cad91c4572b6edd6a6e16ff56c35ed168385
Cory Kelley <coryk0789@gmail.com>
2021-04-05 17:33:59 -0400

    setup basic routing, added some styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from '@chakra-ui/react';
 import Home from './Home';
 import Task from './components/Task';
+import Layout from './components/Layout';
 
 function App() {
 	return (
 		<>
-			<Container>
-				<Router>
-					<Switch>
-						<Route path='/' exact>
-							<Home />
-						</Route>
-						<Route path='/tasks/:id'>
-							<Task />
-						</Route>
-					</Switch>
-				</Router>
-			</Container>
+			<Router>
+				<Layout>
+					<Container maxW='1200'>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/tasks/:id'>
+								<Task />
+							</Route>
+						</Switch>
+					</Container>
+				</Layout>
+			</Router>
 		</>
 	);
 }
---
+++ client/src/components/Layout.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@chakra-ui/button';
+import { Box, Container, Flex, Heading, Spacer } from '@chakra-ui/layout';
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Container
+				maxW='full'
+				borderBottom='1px'
+				borderBottomColor='gray.200'
+				py='6'
+				mb='4'
+			>
+				<Flex maxW='1200' mx='auto'>
+					<Box p='2'>
+						<Link to='/'>
+							<Heading size='md'>Pomlog</Heading>
+						</Link>
+					</Box>
+					<Spacer />
+					<Box>
+						<Button colorScheme='teal' mr='4'>
+							Sign Up
+						</Button>
+						<Button colorScheme='teal'>Log in</Button>
+					</Box>
+				</Flex>
+			</Container>
+			{children}
+		</>
+	);
+};
+
+export default Layout;
