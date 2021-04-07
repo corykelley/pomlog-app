1c995d56a39d5b3b49d16f10a8d6c208478545bd
Cory Kelley <coryk0789@gmail.com>
2021-04-07 16:35:52 -0400

    added modal to homepage to addtasks

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,32 @@
 import { useState, useEffect } from 'react';
-import AddTask from './components/AddTask';
+import Modal from 'react-modal';
 import TaskList from './components/TaskList';
+import AddTask from './components/AddTask';
+
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
+};
+
+Modal.setAppElement('#root');
 
 function Home() {
 	const [tasks, setTasks] = useState(null);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+
+	function openModal() {
+		setModalIsOpen(true);
+	}
+
+	const closeModal = () => {
+		setModalIsOpen(false);
+	};
 
 	const getTasks = () => {
 		fetch(`/api/tasks/`)
@@ -15,6 +38,7 @@ function Home() {
 	};
 
 	const taskSubmit = (method, data, id) => {
+		closeModal();
 		fetch(`/api/tasks/new/${id || ''}`, {
 			method: method,
 			headers: {
@@ -45,7 +69,16 @@ function Home() {
 	return (
 		<>
 			<TaskList tasks={tasks} taskDelete={taskDelete} />
-			<AddTask taskSubmit={taskSubmit} />
+			<button onClick={openModal}>Add Task</button>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel='Example Modal'
+			>
+				<AddTask taskSubmit={taskSubmit} />
+				<button onClick={closeModal}>close</button>
+			</Modal>
 		</>
 	);
 }
